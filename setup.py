a42a5232dfa921e2b0d64e87baffda00f9cc9532
Jérôme Quintard <jquintard@ubikyo.com>
2026-08-19 22:09:08 +0000

    Divers ajouts

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="kbrd-api",
     version="1.0.0",
-    packages=find_packages(include=["kbrd_api", "kbrd_api.*"]),
+
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+
     entry_points={
         "console_scripts": [
             "kbrd-api = kbrd_api.main:main",
